f212dbbdabcb53375ec41bba92c44ea128ae0d2f
Arutyun2312 <arutyunef@gmail.com>
2025-07-08 11:33:22 +0200

    track /cv

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {devIndicators:false}
+const nextConfig: NextConfig = {
+  devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/:locale/cv',
+        destination: '/:locale',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
