6d470deac0545e537e4d1ff0e68e9892afffe4b1
Aru <arutyunef@gmail.com>
2025-11-09 06:19:48 +0100

    fix types

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,17 @@
-import z from 'zod'
-
 export function css(...args: unknown[]) {
   return args.filter((v) => typeof v == 'string').join(' ')
 }
 
 export const pageView = (url: string, measurementId: string) => {
-  window.gtag('config', measurementId, { page_path: url })
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const gtag = (window as any).gtag || function () {}
+  gtag('config', measurementId, { page_path: url })
 }
 
-export const event = ({ action, category, label, value }: any) => {
-  window.gtag('event', action, {
+export const event = ({ action, category, label, value }: { action: string; category: string; label: string; value: number }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const gtag = (window as any).gtag || function () {}
+  gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
